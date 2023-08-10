8368c78b31e218e7bbb2ab7dc35aa0d73987b1d0
cernevv <laura.cernev24@gmail.com>
2023-08-10 13:36:00 -0300

    PHPaula1

---
+++ Aula01/sintaxe3.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo                    "testando espaço em branco <br>";
+    echo "testando espaço <br>"         ;
+
+    // O caracter &nbso; adiciona um espaço em branco dentro da string
+    echo "&#160;  &#160;                              testando  espaço  em  branco <br>";
+    echo
+    "quebra de linha <br>"
+    ;
+
+    echo "quebra de linha <br>"
+    ;
+
+?>
